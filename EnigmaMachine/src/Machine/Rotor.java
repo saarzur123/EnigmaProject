bcd3963a4728bd541342d58a806c291dff65e628
saarzur123 <101362975+saarzur123@users.noreply.github.com>
2022-07-30 12:48:38 +0300

    change modify

--- EnigmaMachine/src/Machine/Rotor.java
+++ EnigmaMachine/src/Machine/Rotor.java
@@ -1,9 +1,12 @@
 package Machine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rotor {
     private int id;
     private final int Window = 0;
-//    private map KeyByCharRight;
+    private Map<Character,Integer> KeyByCharRight = new HashMap<>();
 //    map KeyByIntRight;
 //    map KeyByCharLeft;
 //    map KeyByIntLeft;
