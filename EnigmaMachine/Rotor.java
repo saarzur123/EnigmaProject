8610df6eab21c4de72ad49f962951aae8127ed42
saarzur123 <101362975+saarzur123@users.noreply.github.com>
2022-07-30 12:39:41 +0300

    adding machine module

--- EnigmaMachine/Rotor.java
+++ EnigmaMachine/Rotor.java
@@ -1,2 +1,10 @@
 public class Rotor {
+    private int id;
+    private final int Window = 0;
+    private map KeyByCharRight
+    map KeyByIntRight
+    map KeyByCharLeft
+    map KeyByIntLeft
+    char notchSign
+    bool isForward
 }
