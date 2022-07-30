c3a6410f54f839fc16e9e02ccc26faa467245955
saarzur123 <101362975+saarzur123@users.noreply.github.com>
2022-07-30 18:51:52 +0300

    change rotor members

--- EnigmaMachine/src/Machine/Rotor.java
+++ EnigmaMachine/src/Machine/Rotor.java
@@ -1,22 +1,40 @@
 package Machine;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Rotor {
     private int id;
     private int numberOfCharsInABC;
     private final int Window = 0;
     private Map<Character,Integer> KeyByCharRight = new HashMap<>();
-    private Map<Integer, Character> KeyByIntRight = new HashMap<>();
+    private List<Character> KeyByIntRight = new ArrayList<>();
     private Map<Character,Integer> KeyByCharLeft = new HashMap<>();
-    private Map<Integer, Character> KeyByIntLeft = new HashMap<>();
-    private char notchSign;
+    private List<Character> KeyByIntLeft = new ArrayList<>();
+    private int notchPosition;
     private boolean isForward;
 
 
-    private void InitRotor(int idInput,int notch, String right, String left){
+    private void initRotor(int idInput, int langCount,int notch, String right, String left){
+    id = idInput;
+    numberOfCharsInABC = langCount;
+    notchPosition = notch;
 
+    }
+
+    private void initMapByCharKey(String dataOfChars, Map<Character, Integer> currentMap){
+        int size = dataOfChars.length();
+
+        for (int i = 0; i < size; i++) {
+         currentMap.put(dataOfChars.charAt(i),i);
+        }
+    }
+
+    private void initMapByIndexKey(String dataOfChars, List<Character> currentMap){
+        int size = dataOfChars.length();
+
+        for (int i = 0; i < size; i++) {
+            currentMap.add(i,dataOfChars.charAt(i));
+        }
     }
 
     private void movePositions(Boolean moveRotor){
@@ -26,7 +44,7 @@ public class Rotor {
     }
 
     private char getNotch(){
-        return notchSign;
+        return 'c';
     }
     private char FindCharInNextRotor(int index){
         return 'c';
