5ba02b4d6451553675ee148b080926b2322520d7
saarzur123 <101362975+saarzur123@users.noreply.github.com>
2022-07-30 12:45:14 +0300

    adding rotor

---
+++ EnigmaMachine/src/Machine/Rotor.java
@@ -0,0 +1,12 @@
+package Machine;
+
+public class Rotor {
+    private int id;
+    private final int Window = 0;
+//    private map KeyByCharRight;
+//    map KeyByIntRight;
+//    map KeyByCharLeft;
+//    map KeyByIntLeft;
+//    char notchSign;
+//    bool isForward;
+}
