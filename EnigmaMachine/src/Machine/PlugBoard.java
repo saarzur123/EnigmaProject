c1853a7989a7a8828f12ceba110dd2a4b7b3ea83
natalializi <natalializi@gmail.com>
2022-07-30 13:02:24 +0300

    create PlugBoard and method for Rotor :)

---
+++ EnigmaMachine/src/Machine/PlugBoard.java
@@ -0,0 +1,15 @@
+package Machine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlugBoard {
+    private Map<Character,Character> plugSwitch = new HashMap<>();
+
+    public PlugBoard(){
+
+    }
+    private char checkSwappingChar(char checkSign){
+        return 's';
+    }
+}
--- EnigmaMachine/src/Machine/Rotor.java
+++ EnigmaMachine/src/Machine/Rotor.java
@@ -1,5 +1,7 @@
 package Machine;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,7 +14,26 @@ public class Rotor {
 //    map KeyByIntLeft;
 //    char notchSign;
 //    bool isForward;
+    public Rotor(){
+
+    }
     private void InitRotor(int id, int notch, String right, String left){
+
+    }
+    private void movePositions(Boolean moveRotor){
+
+    }
+
+    private int getNotch(){
+        return 0;
+    }
+    private char FindCharInNextRotor(int index){
+        return 'c';
+    }
+    private int FindIndexInRotor(char charInRotor){
+        return 1;
+    }
+    private void setRotorStartPositionByWindow(int index){
 
     }
 }
