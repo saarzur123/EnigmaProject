f257d543bab242acc614861091475aa083c7e55b
saarzur123 <101362975+saarzur123@users.noreply.github.com>
2022-08-03 09:22:40 +0300

    adding machine class + adding decoding function to rotor

--- EnigmaMachine/src/main.java
+++ EnigmaMachine/src/main.java
@@ -74,6 +74,6 @@ public class main {
 //        machine.insertPlug("A", "F");
 
 
-        System.out.println(machine.encodingAndDecoding("AAAEEEBBBDDDCCCFFF"));
+        System.out.println(machine.encodingAndDecoding("AABBCCDDEEFF"));
     }
 }
