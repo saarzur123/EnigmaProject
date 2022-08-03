fb3e279503505006a702cc23f37df7fdfc93375a
natalializi <natalializi@gmail.com>
2022-08-03 20:40:41 +0300

    update

--- Engine/src/CheckXMLFile/CheckXML.java
+++ Engine/src/CheckXMLFile/CheckXML.java
@@ -1,4 +1,42 @@
 package CheckXMLFile;
 
+import ImportFromXML.XMLToObject;
+import Machine.MachineImplement;
+
+import java.io.File;
+
 public class CheckXML {
+
+    private boolean checkIfTheFileExist(String path){
+        File file = new File(path);
+        return file.exists();
+    }
+
+    private boolean checkFileEnding(String path){
+        if(path.charAt(path.length()-1)=='l'&&path.charAt(path.length()-2)=='m'&&path.charAt(path.length()-3)=='x'){
+            return  true;
+        }
+        return false;
+    }
+    private boolean checkEvenNumberInABC(String ABC) {
+        if(ABC.length() % 2 == 0){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkEnoughRotors(int inUseRotorsNumbers, int rotorsNumber){
+    return rotorsNumber >= inUseRotorsNumbers;
+    }
+
+    private boolean checkRotorsCount(int rotorCount){
+        return rotorCount >=2;
+    }
+
+    private  boolean checkIfAllRotorsHaveUniqueID(){
+        
+    }
+
+
+
 }
