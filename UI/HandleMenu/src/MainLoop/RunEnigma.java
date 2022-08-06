02ecbe7477645fd9a5b6c5f4c33e168703fdafdf
natalializi <natalializi@gmail.com>
2022-08-06 15:38:26 +0300

    update

--- UI/HandleMenu/src/MainLoop/RunEnigma.java
+++ UI/HandleMenu/src/MainLoop/RunEnigma.java
@@ -63,9 +63,10 @@ public class RunEnigma {
                 if (historyAndStatisticsForMachine.getHistoryAndStatSize() == 0) {
                     System.out.println("Unfortunately, there is no option to perform the selected action because there is no machine currently running");
                 }
+                ans = 7;
 
         }
-        return 0;
+        return ans;
     }
 
 }
