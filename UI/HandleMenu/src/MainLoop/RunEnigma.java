1d9272640eca0a92d05ee774b60978e73d1d2686
natalializi <natalializi@gmail.com>
2022-08-07 17:55:01 +0300

    update

--- UI/HandleMenu/src/MainLoop/RunEnigma.java
+++ UI/HandleMenu/src/MainLoop/RunEnigma.java
@@ -7,6 +7,7 @@ import Machine.MachineImplement;
 import Machine.Rotor;
 import MachineDetails.MachineDetails;
 import MachineDetails.SecretCode;
+import RandomAutomation.SecretCodeRandomAutomation;
 
 import java.util.*;
 
@@ -16,6 +17,7 @@ public class RunEnigma {
     private MachineDetails machineDetailsPresenter;
     private MenuHandler menu = new MenuHandler();
     private HistoryAndStatisticsForMachine historyAndStatisticsForMachine = new HistoryAndStatisticsForMachine();
+    private SecretCodeRandomAutomation secretCodeRandomAutomation;
 
     public void Run(){
         int userInput;
@@ -101,65 +103,6 @@ public class RunEnigma {
     }
 
     private void getSecretCodeAutomation(){
-        secretCode = new SecretCode(machine);
-
-        secretCode.determineSecretCode(getRandomPositionForRotors(), getRandomStartingPostChar(), getRandomReflectorID(), getRandomPlugBoard());
-        machineDetailsPresenter.addSecretCode(secretCode);
-        historyAndStatisticsForMachine.addSecretCodeToMachineHistory(secretCode);
-    }
-
-    private List<Integer> getRandomPositionForRotors(){
-        List<Integer> rotorIDPos = new ArrayList<>();
-        Random rand = new Random();
-        Map<Integer, Rotor> rotorMap = machine.getAvailableRotors();
-        int numberOfRotorToChoose = machine.getInUseRotorNumber();
-        for(int i = 0; i < numberOfRotorToChoose; i++){
-            int IDToAdd = rand.nextInt(numberOfRotorToChoose);
-            if(rotorIDPos.contains(IDToAdd))
-                i--;
-            else
-                rotorIDPos.add(IDToAdd);
-        }
-        return rotorIDPos;
-    }
-
-    private List<Character> getRandomStartingPostChar(){
-        List<Character> ListOfStartingPos = new ArrayList<>();
-        int lengthOfABC = machine.getABC().length();
-        Random rand = new Random();
-        for(int k = 0; k < lengthOfABC; k++){
-            ListOfStartingPos.add(machine.getABC().charAt(rand.nextInt(lengthOfABC)));
-        }
-        return ListOfStartingPos;
-    }
-
-    private int getRandomReflectorID(){
-        int numberOfReflector = machine.getAvailableReflectors().size();
-        Random rand = new Random();
-        return rand.nextInt(numberOfReflector);
-    }
-
-    private Map<Character, Character> getRandomPlugBoard(){
-        Map<Character, Character> plugBoardRand = new HashMap<>();
-        int numberOfOptionToChooseFrom = machine.getABC().length() / 2;
-        Random rand = new Random();
-        int numberOfOptionToChooseFromToAdd= rand.nextInt(numberOfOptionToChooseFrom);
-        for(int i = 0; i< numberOfOptionToChooseFromToAdd; i++)
-        {
-            char firstChar = machine.getABC().charAt(rand.nextInt(numberOfOptionToChooseFrom * 2));
-            char secondChar;
-            do {
-                secondChar = machine.getABC().charAt(rand.nextInt(numberOfOptionToChooseFrom * 2));
-            }while (firstChar != secondChar); //כל עוד הם זהים לבחור מישהו אחר
-
-            plugBoardRand.put(firstChar, secondChar);
-            plugBoardRand.put(secondChar, firstChar);
-        }
-        for(int i = 0 ; i < machine.getABC().length(); i++){
-            if(!plugBoardRand.containsKey(machine.getABC().charAt(i))){
-                plugBoardRand.put(machine.getABC().charAt(i),machine.getABC().charAt(i));
-            }
-        }
-        return plugBoardRand;
+        secretCodeRandomAutomation.getSecretCodeAutomation(machine, secretCode, machineDetailsPresenter, historyAndStatisticsForMachine);
     }
 }
