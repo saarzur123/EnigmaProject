e8a37a5f4b08a176bba2775a95a0c80ebd3b6c01
natalializi <natalializi@gmail.com>
2022-09-02 15:41:03 +0300

    update

--- UIDesktop/src/subComponent/main/machine/detail/MachineDetailsController.java
+++ UIDesktop/src/subComponent/main/machine/detail/MachineDetailsController.java
@@ -3,6 +3,7 @@ package subComponent.main.machine.detail;
 import dTOUI.DTOMachineDetails;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import subComponent.main.app.MainAppController;
 import subComponent.main.app.MainScreenController;
 
@@ -11,6 +12,8 @@ import javax.xml.soap.Text;
 public class MachineDetailsController {
     private MainScreenController mainController;
     @FXML
+    private VBox machineDetailsVBox;
+    @FXML
     private Label machineDetailsLBL;
     public void setMainController(MainScreenController main){
         mainController = main;
@@ -18,7 +21,15 @@ public class MachineDetailsController {
 
     public void setMachineDetailsLBL(){
         DTOMachineDetails dtoMachineDetails = mainController.getEngine().getMachineDetailsPresenter().createCurrMachineDetails();
-        String machineDetails = String.format("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh %s",mainController.getEngineCommand().showLastMachineDetails(dtoMachineDetails));
-        machineDetailsLBL.setText(machineDetails);
+        String machineDetails = String.format("%s",mainController.getEngineCommand().showLastMachineDetails(dtoMachineDetails));
+        //machineDetailsLBL.setText(machineDetails);
+        int size = machineDetails.length(), i;
+
+        for (i = 0; i < size; i++) {
+            Label label = new Label();
+            label.setText(machineDetails.substring(i,i + 7));
+            i +=7;
+            machineDetailsVBox.getChildren().add(label);
+        }
     }
 }
