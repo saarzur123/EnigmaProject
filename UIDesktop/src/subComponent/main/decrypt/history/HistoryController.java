6ba5aa6dfc0a1748ef18e595baf51ffa68d78c60
natalializi <natalializi@gmail.com>
2022-09-03 13:37:00 +0300

    update

---
+++ UIDesktop/src/subComponent/main/decrypt/history/HistoryController.java
@@ -0,0 +1,11 @@
+package subComponent.main.decrypt.history;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+public class HistoryController {
+
+    @FXML
+    private VBox historyVbox;
+
+}
