6b0172c04b32038c5a142bc53dccb968ad23ba8f
natalializi <natalializi@gmail.com>
2022-09-03 14:10:23 +0300

    update

---
+++ UIDesktop/src/subComponent/main/decrypt/Restart/RestartSecretCodeController.java
@@ -0,0 +1,13 @@
+package subComponent.main.decrypt.Restart;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class RestartSecretCodeController {
+
+    @FXML
+    void restartSecretCodeBTN(ActionEvent event) {
+
+    }
+
+}
