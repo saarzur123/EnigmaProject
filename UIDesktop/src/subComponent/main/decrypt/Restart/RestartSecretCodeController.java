ae250f379802128cdc1a6772c7dde95d73ade137
natalializi <natalializi@gmail.com>
2022-09-03 14:31:31 +0300

    update

--- UIDesktop/src/subComponent/main/decrypt/Restart/RestartSecretCodeController.java
+++ UIDesktop/src/subComponent/main/decrypt/Restart/RestartSecretCodeController.java
@@ -2,12 +2,19 @@ package subComponent.main.decrypt.Restart;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import subComponent.main.app.MainScreenController;
 
 public class RestartSecretCodeController {
 
+    private MainScreenController mainController;
+
+    public void setMainController(MainScreenController main){
+        mainController = main;
+    }
+
     @FXML
     void restartSecretCodeBTN(ActionEvent event) {
-
+        mainController.getEngineCommand().validateUserChoiceAndResetSecretCode();
     }
 
 }
