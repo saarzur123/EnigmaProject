41211f166182ffa3744bfd63a34d894dd036166a
saarzur123 <101362975+saarzur123@users.noreply.github.com>
2022-09-08 18:38:35 +0300

    d

--- UIDesktop/src/subComponent/main/light.java
+++ UIDesktop/src/subComponent/main/light.java
@@ -29,9 +29,10 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
-public class light{
+public class light {
 
 
     /**
@@ -39,34 +40,35 @@ public class light{
      */
 
 
-
-        public static void main(String[] args) {
-            int size =3;
-            List<Integer> currInd = new ArrayList<>();
-            currInd.add(1);
-            String[] check = {"A","B","C","D","E"};
-            char[] pool = new char[]{'A', 'B', 'C', 'D'};
-            int[] arr = makeBruteForce(3,pool,new int[] {0,0,3},10);
+    public static void main(String[] args) {
+        int size = 3;
+        List<Integer> currInd = new ArrayList<>();
+        currInd.add(1);
+        String[] check = {"A", "B", "C", "D", "E"};
+        char[] pool = new char[]{'A', 'B', 'C', 'D'};
+        int[] arr = new int []{1,2,3};
+        List<List<Integer>> rotorIdCombination = possibleRotorIdPositions(arr);
+        int[] arr1 = makeBruteForce(3, pool, new int[]{0, 0, 3}, 10);
 //            for (int t = 0; t < arr.length; t++) {
 //                            String d = String.format("%s",arr[t]);
 //                            System.out.println(d);
 //                        }
-        }
+    }
 
-    private static int[] makeBruteForce(int length, char[] pool,int[] indexes,int missionSize) {
-        String word="";
+    private static int[] makeBruteForce(int length, char[] pool, int[] indexes, int missionSize) {
+        String word = "";
         int wordIndex = 0;
         List<String> allStartPos = new ArrayList<>();
         // In Java all values in new array are set to zero by default
         // in other languages you may have to loop through and set them.
 
         int pMax = pool.length;  // stored to speed calculation
-        while (indexes[0] < pMax && wordIndex<missionSize) { //if the first index is bigger then pMax we are done
-            word="";
+        while (indexes[0] < pMax && wordIndex < missionSize) { //if the first index is bigger then pMax we are done
+            word = "";
             // print the current permutation
             for (int i = 0; i < length; i++) {
                 System.out.print(pool[indexes[i]]);//print each character
-                word+=pool[indexes[i]];
+                word += pool[indexes[i]];
             }
             System.out.println(); //print end of line
             wordIndex++;
@@ -82,6 +84,33 @@ public class light{
         return indexes;
     }
 
+    public static List<List<Integer>> possibleRotorIdPositions(int[] num) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
 
+        //start from an empty list
+        result.add(new ArrayList<Integer>());
+        List<List<Integer>> current = new ArrayList<List<Integer>>();
+        for (int i = 0; i < num.length; i++) {
+            //list of list in current iteration of the array num
+            current.clear();
+            for (List<Integer> l : result) {
+                // # of locations to insert is largest index + 1
+                for (int j = 0; j < l.size()+1; j++) {
+                    // + add num[i] to different locations
+                    l.add(j, num[i]);
+                    ArrayList<Integer> temp = new ArrayList<Integer>(l);
+                    current.add(temp);
+                    l.remove(j);
+                }
+            }
+            result = new ArrayList<List<Integer>>(current);
+        }
+        System.out.println(result.toString());
+        return result;
     }
+
+
+
+
+}
 
