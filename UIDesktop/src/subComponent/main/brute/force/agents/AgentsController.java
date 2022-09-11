9fb851c231ca6a0e1d4bb70136afe68340010400
saarzur123 <101362975+saarzur123@users.noreply.github.com>
2022-09-11 13:23:24 +0300

    d

--- UIDesktop/src/subComponent/main/brute/force/agents/AgentsController.java
+++ UIDesktop/src/subComponent/main/brute/force/agents/AgentsController.java
@@ -1,6 +1,7 @@
 package subComponent.main.brute.force.agents;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.input.DragEvent;
@@ -9,11 +10,11 @@ import subComponent.main.app.MainScreenController;
 import java.awt.event.MouseEvent;
 
 public class AgentsController {
-    @FXML
-    private Label numberOfAgents;
-
-    @FXML
-    private Slider agentsSlider;
+    @FXML    private Label numberOfAgents;
+    @FXML    private ComboBox<Integer> difficultyLevelCB;
+    @FXML    private Slider agentsSlider;
+    private Integer missionSize = -1;
+    private Integer difficultLevel = -1;
     private MainScreenController mainController;
     public void setMainController(MainScreenController main){
         mainController = main;
@@ -27,8 +28,22 @@ public class AgentsController {
         });
     }
 
+    public Integer getDifficultLevel() {
+        return difficultLevel;
+    }
+
+    public Integer getMissionSize() {
+        return missionSize;
+    }
+
     public void setAgentsMaxSlider() {
         agentsSlider.setMax(mainController.getEngine().getDecryptionManager().getAgentNumber());
+    }
+
+    private void setDifficultyLevelCB(){
+        for (int i = 1; i <= 4; i++) {
+            difficultyLevelCB.getItems().add(i);
+        }
     }
 }
 
