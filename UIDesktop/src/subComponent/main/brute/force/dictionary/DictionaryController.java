599b8601c3c30c108f62a9b984e6857ce1ffb632
natalializi <natalializi@gmail.com>
2022-09-17 15:49:21 +0300

    update

--- UIDesktop/src/subComponent/main/brute/force/dictionary/DictionaryController.java
+++ UIDesktop/src/subComponent/main/brute/force/dictionary/DictionaryController.java
@@ -1,12 +1,17 @@
 package subComponent.main.brute.force.dictionary;
 
+import com.sun.javafx.scene.control.skin.LabeledText;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.PickResult;
 import javafx.scene.layout.VBox;
 import subComponent.main.app.MainScreenController;
 import subComponent.main.brute.force.dictionary.trie.Trie;
@@ -28,15 +33,32 @@ public class DictionaryController {
     private Trie trie;
     public void SetDictionaryController(){
         List<String>  listAllDictionary = new ArrayList<>();
+        listOfDataVB.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                 PickResult vv =mouseEvent.getPickResult();
+                Node n = vv.getIntersectedNode();
 
+                if(n instanceof LabeledText) {
+                    String currSelected = ((LabeledText) n).getText();
+                    searchTF.setText(currSelected);
+                    System.out.println(currSelected);
+                }
+
+            }
+        });
         for (String s :mainController.getEngine().getDM().getDictionary().getDictionaryFilteredWords()){
             listAllDictionary.add(s);
-            listOfDataVB.getChildren().add(new Label(s));
+            Label LB = new Label(s);
+            listOfDataVB.getChildren().add(LB);
+
         }
 
         trie = new Trie(listAllDictionary);
 
     }
+
+
     @FXML
     public void initialize(){
         searchTF.textProperty().addListener((obs, oldText, newText) -> {
