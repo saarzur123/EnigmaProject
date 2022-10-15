822ba285d850ca905c44273014719fd5ba58028b
Natali Alizi <natalial@mta.ac.il>
2022-10-15 12:18:22 +0300

    d

---
+++ Allies/src/component/agent/data/AgentDataController.java
@@ -0,0 +1,17 @@
+package Component.agent.data;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class AgentDataController {
+
+    @FXML
+    private Label agentNameController;
+
+    @FXML
+    private Label amountOfThreadLBL;
+
+    @FXML
+    private Label missionSizeLBL;
+
+}
